8f782c41b94fa26fae08acfd42c55605cff4eb1f
EMDLAB Package <alijamalifard1992@gmail.com>
2025-07-30 17:47:46 +0330

    Updating permanent magnet material properties

--- materials/emdlab_mlib_pm_Y22H.m
+++ materials/emdlab_mlib_pm_Y22H.m
@@ -3,6 +3,14 @@ classdef emdlab_mlib_pm_Y22H < handle & emdlab_mlib_permanentMagnet
     methods
         
         function obj = emdlab_mlib_pm_Y22H()
+            obj.ThermalConductivity.value = 1;
+            obj.HeatCapacity.value = 1;
+            obj.ElectricPermitivity.value = 0;
+            obj.ElectricConductivity.value = 0;        
+            obj.MagneticPermeability.value = 4*pi*1e-7;
+            obj.MassDensity.value = 1;
+            obj.YoungModulus.value = 10;
+            obj.PoissonRatio.value = 0.2;
         end
         
     end
